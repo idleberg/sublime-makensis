790d78a8827f0bb4b956e183a47abe2061ac1642
idleberg <jan@idleberg.com>
2016-11-22 09:31:31 +0100

    non-fatal errors

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -4,27 +4,28 @@
 # https://github.com/idleberg/sublime-makensis
 
 if (!(Test-Path HKLM:)) {
+    # This should never be shown when run from Sublime Text
     Write-Host "Error: This script runs on PowerShell for Windows only"
     Exit
 }
 
 if ($PSVersionTable.PSVersion.Major -lt 3) {
-    Write-Error "Error: This script requires PowerShell 3 (or higher)"
+    # PowerShell 3.0 is integrated with Windows 8 and with Windows Server 2012
+    Write-Host "Error: This script requires PowerShell 3 (or higher)"
     Exit
 }
 
 if ([System.Environment]::Is64BitOperatingSystem) {
-    $key = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\NSIS'
+    $path = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\NSIS'
 } else {
-    $key = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\NSIS'
+    $path = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\NSIS'
 }
 
-$nsis = (Get-ItemProperty -Path $key -Name InstallLocation).InstallLocation
+$nsis = (Get-ItemProperty -Path $path -Name InstallLocation).InstallLocation
 $makensis = Join-Path -Path $nsis -ChildPath "makensis.exe"
 
 try {
     Start-Process -NoNewWindow -FilePath "$makensis" -ArgumentList "$args"
-
 } catch [System.Management.Automation.CommandNotFoundException] {
-    Write-Error "Error: makensis.exe isn't in your PATH or NSIS_HOME environment variable."
+    Write-Host "Error: makensis.exe isn't in your PATH environment variable."
 }
