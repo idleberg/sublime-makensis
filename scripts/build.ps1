03982f56913394551eee286d55b9201aa51f6b9e
idleberg <jan@idleberg.com>
2016-11-22 13:16:50 +0100

    modify NSIS detection

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -3,29 +3,38 @@
 #
 # https://github.com/idleberg/sublime-makensis
 
-if (!(Test-Path HKLM:)) {
+If (-Not (Test-Path HKLM:)) {
     # This should never be shown when run from Sublime Text
-    Write-Host "Error: This script runs on PowerShell for Windows only"
+    Write-Host "Error: This script is meant to be run on Windows only"
     Exit
 }
 
-if ($PSVersionTable.PSVersion.Major -lt 3) {
+If ($PSVersionTable.PSVersion.Major -lt 3) {
     # PowerShell 3.0 is integrated with Windows 8 and with Windows Server 2012
     Write-Host "Error: This script requires PowerShell 3.0 (or higher)"
     Exit
 }
 
-if ([System.Environment]::Is64BitOperatingSystem) {
-    $path = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\NSIS'
-} else {
-    $path = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\NSIS'
+If ((Test-Path env:NSIS_HOME) -And (Test-Path "${env:NSIS_HOME}\makensis.exe")) {
+    Write-Debug "Checking %NSIS_HOME% for makensis.exe"
+    $makensis = "${env:NSIS_HOME}\makensis.exe"
+} ElseIf (Get-Command "makensis.exe" -ErrorAction SilentlyContinue) {
+    Write-Debug "Checking %PATH% for makensis.exe"
+    $makensis = "makensis.exe"
+} Else {
+    Write-Debug "Checking registry for NSIS"
+    If ([System.Environment]::Is64BitOperatingSystem) {
+        $nsis_key = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\NSIS'
+    } Else {
+        $nsis_key = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\NSIS'
+    }
+
+    $nsis_path = (Get-ItemProperty -Path $nsis_key -Name InstallLocation).InstallLocation
+    $makensis = Join-Path -Path $nsis_path -ChildPath "makensis.exe"
 }
 
-$nsis = (Get-ItemProperty -Path $path -Name InstallLocation).InstallLocation
-$makensis = Join-Path -Path $nsis -ChildPath "makensis.exe"
-
-try {
+Try {
     Start-Process -NoNewWindow -FilePath "$makensis" -ArgumentList "$args"
-} catch [System.Management.Automation.CommandNotFoundException] {
+} Catch [System.Management.Automation.CommandNotFoundException] {
     Write-Host "Error: makensis.exe isn't in your PATH environment variable."
 }
