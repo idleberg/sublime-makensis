e48b13d2bfed0d7d46dc9072a35e282ca45cac1c
Jan T. Sott <jan@idleberg.com>
2017-10-13 02:56:54 +0200

    platform-independent

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,27 +1,22 @@
 # The MIT License (MIT)
-# Copyright (c) 2016 Jan T. Sott
+# Copyright (c) 2016, 2017 Jan T. Sott
 #
 # https://github.com/idleberg/sublime-makensis
 
 $DebugPreference = "SilentlyContinue"
 
-If (-Not (Test-Path HKLM:)) {
-    # This should never be shown when run from Sublime Text
-    Write-Host "Error: This script is meant to be run on Windows only"
-    Exit
-}
-
+# PowerShell 3.0 is integrated with Windows 8 and with Windows Server 2012
 If ($PSVersionTable.PSVersion.Major -lt 3) {
-    # PowerShell 3.0 is integrated with Windows 8 and with Windows Server 2012
     Write-Host "Error: This script requires PowerShell 3.0 (or higher)"
     Exit
 }
 
-If (Get-Command "makensis.exe" -ErrorAction SilentlyContinue) {
-    Write-Debug "'makensis.exe' found in %PATH%"
-    $makensis = "makensis.exe"
-} Else {
-    Write-Debug "Checking registry for NSIS installation path"
+If (Get-Command "makensis" -ErrorAction SilentlyContinue) {
+    Write-Debug "'makensis' found in %PATH%"
+    $makensis = "makensis"
+} ElseIf (Test-Path HKLM:) {
+    Write-Debug "Checking Windows Registry for NSIS installation path"
+
     If ([System.Environment]::Is64BitOperatingSystem) {
         $nsis_key = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\NSIS'
     } Else {
